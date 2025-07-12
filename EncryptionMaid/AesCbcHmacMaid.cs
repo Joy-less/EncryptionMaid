d9c7e29b297bc5f4ead96aab42aef8b41348f647
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-07-12 02:14:57 +0100

    Simplify AesCbcHmac

--- EncryptionMaid/AesCbcHmacMaid.cs
+++ EncryptionMaid/AesCbcHmacMaid.cs
@@ -21,29 +21,6 @@ public static class AesCbcHmacMaid {
     /// The maximum number of bytes to dynamically allocate on the stack.
     /// </summary>
     private const int StackAllocMaxSize = 32;
-    /// <summary>
-    /// The size in bytes of the derived encryption key.
-    /// Must be 256 bits (32 bytes) to use HMACSHA256.
-    /// </summary>
-    private const int AesKeySize = 32;
-    /// <summary>
-    /// The size in bytes of the salt for the derived encryption key.
-    /// </summary>
-    private const int AesKeySaltSize = 16;
-    /// <summary>
-    /// The size in bytes of the hash-based message authentication code used to verify the encrypted data wasn't tampered with.
-    /// Must be 256 bits (32 bytes) to use HMACSHA256.
-    /// </summary>
-    private const int HmacSize = 32;
-    /// <summary>
-    /// The size in bytes of the encryption key for the hash-based message authentication code.
-    /// Must be 256 bits (32 bytes) to use HMACSHA256.
-    /// </summary>
-    private const int HmacKeySize = 32;
-    /// <summary>
-    /// The size in bytes of the salt for the encryption key for the hash-based message authentication code.
-    /// </summary>
-    private const int HmacKeySaltSize = 16;
 
     /// <summary>
     /// Converts the plain bytes to encrypted bytes using the given key.
@@ -62,11 +39,12 @@ public static class AesCbcHmacMaid {
         Span<byte> IV = stackalloc byte[IVSize];
         RandomNumberGenerator.Fill(IV);
 
-        Span<byte> AesKeySalt = stackalloc byte[HmacKeySaltSize];
-        RandomNumberGenerator.Fill(AesKeySalt);
+        Span<byte> KeyHash = stackalloc byte[SHA256.HashSizeInBytes];
+        int KeyHashWritten = SHA256.HashData(Key, KeyHash);
+        KeyHash = KeyHash[..KeyHashWritten];
 
-        Span<byte> AesKey = stackalloc byte[HmacKeySize];
-        HKDF.DeriveKey(HashAlgorithmName.SHA256, Key, AesKey, AesKeySalt, default);
+        Span<byte> AesKey = KeyHash[..(SHA256.HashSizeInBytes / 2)];
+        Span<byte> HmacKey = KeyHash[(SHA256.HashSizeInBytes / 2)..];
 
         using Aes Aes = Aes.Create();
         Aes.Key = AesKey.ToArray();
@@ -78,19 +56,13 @@ public static class AesCbcHmacMaid {
         int CipherBytesWritten = Aes.EncryptCbc(PlainBytes, IV, CipherBytes);
         CipherBytes = CipherBytes[..CipherBytesWritten];
 
-        Span<byte> HmacKeySalt = stackalloc byte[HmacKeySaltSize];
-        RandomNumberGenerator.Fill(HmacKeySalt);
+        byte[] HmacInput = [.. IV, .. CipherBytes];
 
-        Span<byte> HmacKey = stackalloc byte[HmacKeySize];
-        HKDF.DeriveKey(HashAlgorithmName.SHA256, Key, HmacKey, HmacKeySalt, default);
-
-        byte[] HmacInput = [.. AesKeySalt, .. IV, .. CipherBytes];
-
-        Span<byte> HmacBytes = stackalloc byte[HmacSize];
+        Span<byte> HmacBytes = stackalloc byte[HMACSHA256.HashSizeInBytes];
         int HmacBytesWritten = HMACSHA256.HashData(HmacKey, HmacInput, HmacBytes);
         HmacBytes = HmacBytes[..HmacBytesWritten];
 
-        byte[] EncryptedBytes = [.. AesKeySalt, .. IV, .. CipherBytes, .. HmacKeySalt, .. HmacBytes];
+        byte[] EncryptedBytes = [.. IV, .. CipherBytes, .. HmacBytes];
         return EncryptedBytes;
     }
     /// <summary>
@@ -128,28 +100,28 @@ public static class AesCbcHmacMaid {
     /// The decrypted bytes.
     /// </returns>
     /// <exception cref="CryptographicException"/>
+    /// <exception cref="AuthenticationTagMismatchException"/>
     public static byte[] Decrypt(scoped ReadOnlySpan<byte> EncryptedBytes, scoped ReadOnlySpan<byte> Key) {
-        ReadOnlySpan<byte> AesKeySalt = EncryptedBytes[..AesKeySaltSize];
-        ReadOnlySpan<byte> IV = EncryptedBytes[AesKeySaltSize..(AesKeySaltSize + IVSize)];
-        ReadOnlySpan<byte> CipherBytes = EncryptedBytes[(AesKeySaltSize + IVSize)..^(HmacKeySaltSize + HmacSize)];
-        ReadOnlySpan<byte> HmacKeySalt = EncryptedBytes[^(HmacKeySaltSize + HmacSize)..^HmacSize];
-        ReadOnlySpan<byte> HmacBytes = EncryptedBytes[^HmacSize..];
+        ReadOnlySpan<byte> IV = EncryptedBytes[..IVSize];
+        ReadOnlySpan<byte> CipherBytes = EncryptedBytes[IVSize..^HMACSHA256.HashSizeInBytes];
+        ReadOnlySpan<byte> HmacBytes = EncryptedBytes[^HMACSHA256.HashSizeInBytes..];
 
-        Span<byte> HmacKey = stackalloc byte[HmacKeySize];
-        HKDF.DeriveKey(HashAlgorithmName.SHA256, Key, HmacKey, HmacKeySalt, default);
+        Span<byte> KeyHash = stackalloc byte[SHA256.HashSizeInBytes];
+        int KeyHashWritten = SHA256.HashData(Key, KeyHash);
+        KeyHash = KeyHash[..KeyHashWritten];
 
-        byte[] HmacInput = [.. AesKeySalt, .. IV, .. CipherBytes];
+        Span<byte> AesKey = KeyHash[..(SHA256.HashSizeInBytes / 2)];
+        Span<byte> HmacKey = KeyHash[(SHA256.HashSizeInBytes / 2)..];
 
-        Span<byte> TestHmacBytes = stackalloc byte[HmacSize];
+        byte[] HmacInput = [.. IV, .. CipherBytes];
+
+        Span<byte> TestHmacBytes = stackalloc byte[HMACSHA256.HashSizeInBytes];
         int TestHmacBytesWritten = HMACSHA256.HashData(HmacKey, HmacInput, TestHmacBytes);
         TestHmacBytes = TestHmacBytes[..TestHmacBytesWritten];
 
         if (!CryptographicOperations.FixedTimeEquals(HmacBytes, TestHmacBytes)) {
             throw new AuthenticationTagMismatchException();
         }
-
-        Span<byte> AesKey = stackalloc byte[HmacKeySize];
-        HKDF.DeriveKey(HashAlgorithmName.SHA256, Key, AesKey, AesKeySalt, default);
 
         using Aes Aes = Aes.Create();
         Aes.Key = AesKey.ToArray();
@@ -174,6 +146,7 @@ public static class AesCbcHmacMaid {
     /// The decrypted bytes.
     /// </returns>
     /// <exception cref="CryptographicException"/>
+    /// <exception cref="AuthenticationTagMismatchException"/>
     public static string Decrypt(scoped ReadOnlySpan<byte> EncryptedBytes, string Password) {
         byte[] Key = SHA256.HashData(Encoding.UTF8.GetBytes(Password));
         byte[] PlainBytes = Decrypt(EncryptedBytes, Key);
